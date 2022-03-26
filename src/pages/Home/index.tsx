298e4ac0c1fcd5b662658af6c6734a04c31aaf02
visko93 <vskonicezny@gmail.com>
2022-03-25 23:16:50 -0300

    feat: add Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
+import Image from "next/image";
 
 import styles from "./style.module.css";
 
 function HomePage() {
   return (
-    <div className={styles.container}>
-      <h1>Home</h1>
-    </div>
+    <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <section className={styles.section}>
+          <Image
+            src={"/vanhack-small.svg"}
+            alt="A letter V and H simbolizing the company name: Van Hack"
+            width={136}
+            height={29}
+          />
+        </section>
+        <section className={styles.section}>
+          {/* Tabs */}
+          <div>
+            <span>tab1</span>
+            <span>tab2</span>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
 
